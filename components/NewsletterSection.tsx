3691d466bd09d4172da62309ce31a1e3c2724501
gr1ntch <odewumimighty@gmail.com>
2025-02-13 23:20:26 +0100

    Changed Send button

--- components/NewsletterSection.tsx
+++ components/NewsletterSection.tsx
@@ -1,4 +1,4 @@
-import { Send } from "lucide-react"
+import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
@@ -7,10 +7,17 @@ export function NewsletterSection() {
     <section className="py-12 lg:py-24">
       <div className="sm:container">
         <div className="mx-auto max-w-xl">
-          <div className="flex items-center gap-4">
-            <Input type="email" placeholder="Enter your email" className="bg-transparent" />
-            <Button className="rounded-tl-xl rounded-tr-xl rounded-bl-xl" variant="primary" size="lg">
-              <Send className="h-4 w-4" />
+          <div className="relative flex items-center">
+            <Input 
+              type="email" 
+              placeholder="Enter your email" 
+              className="bg-transparent pr-16"
+            />
+            <Button 
+              className="absolute right-1 bg-white rounded-full" 
+              size="send"
+            >
+              <Image src="/sendMail.svg" alt="Send icon" width={16} height={16} />
               <span className="sr-only">Subscribe</span>
             </Button>
           </div>
@@ -19,4 +26,3 @@ export function NewsletterSection() {
     </section>
   )
 }
-
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "default" | "primary" | "secondary" | "outline" | "outlineBlue"
-  size?: "sm" | "md" | "lg"
+  size?: "sm" | "md" | "lg" | "send"
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -20,13 +20,14 @@ export const Button: React.FC<ButtonProps> = ({
     primary: "bg-blue-600 text-white hover:bg-blue-700",
     secondary: "bg-green-500 text-white hover:bg-green-600",
     outline: "border border-gray-300 hover:bg-red-500 rounded-tr-[30px] rounded-b-[30px] text-white",
-    outlineBlue: "bg-[#1A73E8] border border-gray-300 hover:bg-blue-400 rounded-tr-[30px] rounded-b-[30px] text-white"
+    outlineBlue: "bg-[#1A73E8] border border-gray-300 hover:bg-blue-400 rounded-tr-[30px] rounded-b-[30px] text-white",
   }
 
   const sizes = {
     sm: "h-8 px-3 text-sm rounded-tl-md rounded-tr-md rounded-bl-md",
     md: "h-10 px-4 text-base rounded-tl-lg rounded-tr-lg rounded-bl-lg",
     lg: "h-12 px-6 text-lg ",
+    send: "h-10 px-3"
   }
 
   const variantStyles = variants[variant]
@@ -38,4 +39,3 @@ export const Button: React.FC<ButtonProps> = ({
     </button>
   )
 }
-
